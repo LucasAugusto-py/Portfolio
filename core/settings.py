b2b1dd579bfc221df647b13db80c6437c84dfdcb
lucas_augusto_silva <85196283+LucasAugusto-py@users.noreply.github.com>
2023-10-23 10:43:57 -0300

    added RENDER hostname

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,10 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
