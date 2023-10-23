0622cf48056d13922200568d747d1d0e7172b27c
lucas_augusto_silva <85196283+LucasAugusto-py@users.noreply.github.com>
2023-10-23 10:50:18 -0300

    fixed ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='my secret key')
 
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS') if DEBUG else []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
